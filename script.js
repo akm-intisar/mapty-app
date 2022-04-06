6d865f9205978383cd4a3b4442e57e9e36bf9946
AKM Intisar Islam <akm.intisar@gmail.com>
2022-04-06 21:55:52 +0600

    leaflet-added

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude]).addTo(map).bindPopup('Mahee').openPopup();
     },
     function (position) {
       alert('Please Try Again');
